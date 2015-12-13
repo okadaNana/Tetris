69bc434577c14213f8402a7bcdd30c49ac57a60d
owenmike <xiaotu.dev@gmail.com>
2015-12-13 16:13:46 +0800

    GameFrame.java

---
+++ src/com/owen/tetris/main/Main.java
@@ -0,0 +1,14 @@
+package com.owen.tetris.main;
+
+import com.owen.tetris.ui.GameFrame;
+
+/**
+ * Created by mike on 15/12/13.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        new GameFrame().setVisible(true);
+    }
+
+}
---
+++ src/com/owen/tetris/ui/GameFrame.java
@@ -0,0 +1,23 @@
+package com.owen.tetris.ui;
+
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * Created by mike on 15/12/13.
+ */
+public class GameFrame extends JFrame {
+
+    public GameFrame() {
+        this.setTitle("Java俄罗斯方块");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setSize(1200, 700);
+        this.setResizable(false);
+        Toolkit toolkit = Toolkit.getDefaultToolkit();
+        Dimension screen = toolkit.getScreenSize();
+        int x = (screen.width - this.getWidth()) >> 1;
+        int y = (screen.height - this.getHeight()) >> 1;
+        this.setLocation(x, y);
+    }
+
+}
