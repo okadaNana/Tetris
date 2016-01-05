7399060386086fcc4f62771212cb45a3f91a0689
owenmike <xiaotu.dev@gmail.com>
2016-01-05 20:29:06 +0800

    分数值槽原型

--- src/com/owen/tetris/ui/LayerPoint.java
+++ src/com/owen/tetris/ui/LayerPoint.java
@@ -11,11 +11,14 @@ import java.awt.*;
 public class LayerPoint extends Layer {
 
     /**
+     * 分数最大位数
+     */
+    private static final int POINT_BIT = 5;
+
+    /**
      * 标题图片(分数)
      */
     private static final Image IMG_POINT = new ImageIcon("graphics/string/point.png").getImage();
-
-    private static final int POINT_Y = PADDING;
 
     /**
      * 标题图片(消行)
@@ -27,29 +30,57 @@ public class LayerPoint extends Layer {
      */
     private static final int IMG_RMLINE_H = IMG_RMLINE.getHeight(null);
 
-    private static final int IMG_RMLINE_Y =  IMG_RMLINE_H + (PADDING << 1);
+    /**
+     * 消行y坐标
+     */
+    private final int rmLineY;
 
     /**
-     * 分数最大位数
+     * 分数y坐标
      */
-    private static final int POINT_BIT = 5;
+    private final int pointY;
 
-    private static int POINT_X;
+    /**
+     * 经验值y坐标
+     */
+    private final int expY;
+
+    /**
+     * 经验值槽宽度
+     */
+    private final int expW;
+
+    /**
+     * 分数x坐标
+     */
+    private int comX;
 
     public LayerPoint(int x, int y, int w, int h) {
         super(x, y, w, h);
-        // 初始化分数显示的x坐标
-        POINT_X = this.w - IMG_NUMBER_W * POINT_BIT - PADDING;
+        // 初始化共通x坐标
+        this.comX = this.w - IMG_NUMBER_W * POINT_BIT - PADDING;
+        // 初始化分数显示的y坐标
+        this.pointY = PADDING;
+        // 初始化消行显示的y坐标
+        this.rmLineY =  this.pointY + IMG_POINT.getHeight(null) + PADDING;
+        // 初始化经验值y坐标
+        this.expY = this.rmLineY + IMG_RMLINE.getHeight(null) + PADDING;
+        // 初始化经验值槽宽度
+        this.expW = this.w - (PADDING << 1);
     }
 
     public void paint(Graphics g) {
         this.createWindow(g);
         // 窗口标题文字(分数)
-        g.drawImage(IMG_POINT, this.x + PADDING, this.y + POINT_Y, null);
-        this.drawNumber(POINT_X, POINT_Y, this.dto.getNowPoint(), POINT_BIT, g);
+        g.drawImage(IMG_POINT, this.x + PADDING, this.y + pointY, null);
+        // 显示分数
+        this.drawNumber(comX, pointY, this.dto.getNowPoint(), POINT_BIT, g);
         // 窗口标题文字(消行)
-        g.drawImage(IMG_RMLINE, this.x + PADDING, this.y + IMG_RMLINE_Y, null);
-        this.drawNumber(POINT_X, IMG_RMLINE_Y, this.dto.getNowRemoveLine(), POINT_BIT, g);
+        g.drawImage(IMG_RMLINE, this.x + PADDING, this.y + rmLineY, null);
+        // 显示消行
+        this.drawNumber(comX, rmLineY, this.dto.getNowRemoveLine(), POINT_BIT, g);
+        // 绘制值槽(经验值)
+        g.fillRect(this.x + PADDING, this.y + expY, this.expW, 32);
     }
 
 }
